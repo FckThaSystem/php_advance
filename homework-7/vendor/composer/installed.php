32f221c9fc42fbb9ded3c8d4c7c885b94f59a6a0
Rostyslav.Kibkalo <rostislav.kibkalo@gmail.com>
2022-04-21 22:46:15 +0300

    homework-7

---
+++ homework-7/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2fb4784b53c247f87a4141c56720d9e4c6d803b8',
+        'name' => 'rostyslav/my-new-project',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rostyslav/my-new-project' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2fb4784b53c247f87a4141c56720d9e4c6d803b8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
